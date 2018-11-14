cc24e79165190f0cf9a900f2b358213578b67cce
nolan <nolan9b@outlook.com>
2018-11-14 21:19:19 +0800

    Commit Initial Twenty One Commit

---
+++ Tech Academy Console App 1/Tech Academy Console App 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Tech_Academy_Console_App_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
